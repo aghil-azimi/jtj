5f31df4ade49f8ce91ac8571fb796981ae92349a
aghil-azimi <aghil.azimi992@gmail.com>
2023-07-20 11:19:58 +0200

    added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+ 
+};
+
+export default App;
